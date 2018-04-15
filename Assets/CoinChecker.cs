db2243e8c5013fd321d05adfd2c729da6613cac8
Marcos Alcantar <marcosalcantar97@gmail.com>
2018-04-15 15:44:57 -0700

    Coin Checker/Start Position script done

--- Assets/CoinChecker.cs
+++ Assets/CoinChecker.cs
@@ -3,14 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CoinChecker : MonoBehaviour {
+	public int numCoinsInLevel;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+	void OnTriggerEnter2D(Collider2D other) {
+		if ( other.gameObject.CompareTag("Player") ) {
+			if (other.gameObject.GetComponent<Player>().GetPickUpsCount() == numCoinsInLevel) {
+				//Won Game! Yay!
+				Debug.Log("YOU WON");
+			}
+		}
 	}
 }
