6fa7ddece2f533c6b0e8fe0d5b11c71e47709a7f
Joshua Heideman <jcheideman@ucdavis.edu>
2018-04-14 18:25:26 -0700

    Made camera follow player

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 public class Player : MonoBehaviour {
 	[SerializeField]
+	private Camera cam;
+	[SerializeField]
     private Rigidbody2D rb;
 	[SerializeField]
 	private float velocity_x;
@@ -12,16 +14,24 @@ public class Player : MonoBehaviour {
 	private float speed = 5.0f;
 	[SerializeField]
 	private Vector2 total_velocity;
+	private GameObject character;
 	// Use this for initialization
 	void Start () {
 		rb = gameObject.GetComponent<Rigidbody2D>();
+		cam = GameObject.FindGameObjectWithTag ("MainCamera").GetComponent<Camera>();
+		character = this.gameObject;
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void FixedUpdate () {
 		velocity_x=Input.GetAxis ("Horizontal");
 		velocity_y = Input.GetAxis ("Vertical");
 		total_velocity = new Vector2 (velocity_x*speed, velocity_y*speed);
 		rb.velocity = total_velocity;
+	}
+	//Use for Camera stuff
+	void LateUpdate()
+	{
+		cam.transform.position = new Vector3 (rb.gameObject.transform.position.x,rb.gameObject.transform.position.y,0);
 	}
 }
---
+++ Assets/SceneSwitch.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class SceneSwitch : MonoBehaviour {
+	private bool isWaiting;
+	// Use this for initialization
+	void Start () {
+		isWaiting = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (!isWaiting) {
+			StartCoroutine ("WaitTenSeconds");
+		} else {
+			//TODO: Press space to switch to other map.
+		}
+	}
+
+	IEnumerator WaitTenSeconds()
+	{
+		yield return new WaitForSecondsRealtime (10.0f);
+
+	}
+}
